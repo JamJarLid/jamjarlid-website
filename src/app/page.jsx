3bb910ed5f9fc7987b39ec7a832ee18fb87249ec
JamJarLid <espen.lever@gmail.com>
2024-01-24 11:40:09 +0100

    Added links to socials on homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,31 @@
-import Link from "next/link"
-import Image from "next/image";
-import styles from "../styles/page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <Link href="/twitch">
-        <h2>Twitch</h2>
+      <div>
+        <Link href="/twitch">
+          <h2>Twitch</h2>
+        </Link>
         <p>Main streaming site</p>
-      </Link>
-      <Link href="/youtube">
-        <h2>YouTube</h2>
+        <Link href="https://twitch.tv/jamjarlid">To Twitch Channel</Link>
+      </div>
+      <div>
+        <Link href="/youtube">
+          <h2>YouTube</h2>
+        </Link>
         <p>Videos of stream highlights and secondary stream</p>
-      </Link >
-      <Link href="/github">
-        <h2>Github</h2>
+        <Link href="https://www.youtube.com/channel/UCHeK73JvwilHaKwi0_7dpjg">
+          To YouTube Channel
+        </Link>
+      </div>
+      <div>
+        <Link href="/github">
+          <h2>Github</h2>
+        </Link>
         <p>Coding projects and other personal source code</p>
-      </Link >
+        <Link href="https://github.com/JamJarLid">To GitHub Page</Link>
+      </div>
     </main>
   );
 }
